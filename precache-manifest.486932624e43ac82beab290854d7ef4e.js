467d6bd4008819f847e98fd043369ae7cd04e298
Frederik Funke <frederik-funke@gmx.de>
2018-10-31 19:57:27 +0100

    Updates

---
+++ precache-manifest.486932624e43ac82beab290854d7ef4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cfeb8652b0e3987c650",
+    "url": "/fcc_frontend_javascript_calculator/static/css/main.587d9498.chunk.css"
+  },
+  {
+    "revision": "9cfeb8652b0e3987c650",
+    "url": "/fcc_frontend_javascript_calculator/static/js/main.9cfeb865.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/fcc_frontend_javascript_calculator/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "6204062a82a801802323",
+    "url": "/fcc_frontend_javascript_calculator/static/js/runtime~main.6204062a.js"
+  },
+  {
+    "revision": "d65edb29f9e1026f871ce7a63789c713",
+    "url": "/fcc_frontend_javascript_calculator/index.html"
+  }
+];
